6374385eb9fa1837965ddd3ef182da685084917d
Moacir P. de Sá Pereira <moacir@gmail.com>
2015-10-08 23:38:21 -0400

    Add test for about.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,12 +5,16 @@ RSpec.describe App do
   it "should allow accessing the home page" do
     get '/'
     expect(last_response).to be_ok
-    # expect(last_response.body).to eq "Hello World!"
   end
 
   it "should say “This is the Little Lending Library” on the index page" do
     get '/'
     expect(last_response.body).to include "This is the Little Lending Library"
+  end
+
+  it "should have a title that reads “About…” on the about page" do
+    get '/about'
+    expect(last_response.body).to include "<title>About"
   end
 
 end
